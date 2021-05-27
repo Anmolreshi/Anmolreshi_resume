af03c1e42d1bc93fd276542e6aafc9716c40417c
Anmol reshi <55075362+Anmolreshi@users.noreply.github.com>
2021-05-27 15:56:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -420,39 +420,41 @@ function App() {
       </Row>
       <Row className="center space">Follow me on the web</Row>
       <Row className="center space-1">
-                  <Col xl={1} lg={1} sm={2} xs={2}>
+                   <Col md={1}></Col>
+                  <Col xl={1} lg={1} md={1} sm={2} xs={2}>
                     <a href="https://github.com/Anmolreshi">
                     <img className="socialmedia" alt="github" src={github} />
                       </a>
                    </Col>
-                   <Col xl={1} lg={1} sm={2} xs={2}>
+                   <Col xl={1} lg={1} md={1}sm={2} xs={2}>
                     <a href="https://hashnode.com/@anmolreshi">
                     <img className="socialmedia" alt="dev" src={hashnode} />
                       </a>
                   </Col>
-                  <Col xl={1} lg={1} sm={2} xs={2}>
+                  <Col xl={1} lg={1} md={1} sm={2} xs={2}>
                     <a href="mailto:anmolreshi@gmail.com">
                     <img className="socialmedia" alt="email" src={gmail} />
                       </a>
                   </Col>
-                  <Col xl={1} lg={1} sm={2} xs={2}>
+                  <Col xl={1} lg={1}  md={1} sm={2} xs={2}>
                     <a href="https://www.instagram.com/iamanmolreshi_20/">
                     <img className="socialmedia" alt="instagram" src={instagram} />
                       </a>
                   </Col>
-                  <Col xl={1} lg={1} sm={2} xs={2}>
+                  <Col xl={1} lg={1}  md={1} sm={2} xs={2}>
                     <img className="socialmedia" alt="snapchat" src={snapchat} />
                   </Col>
-                  <Col xl={1} lg={1} sm={2} xs={2}>
+                  <Col xl={1} lg={1}  md={1} sm={2} xs={2}>
                       <a href="https://www.linkedin.com/in/anmolreshi/">
                     <img className="socialmedia" alt="linkedin" src={linkedin} />
                       </a>
                   </Col>
-                  <Col xl={1} lg={1} sm={2} xs={2}>
+                  <Col xl={1} lg={1}  md={1} sm={2} xs={2}>
                     <a href="https://twitter.com/AnmolreshiAnmol">
                     <img className="socialmedia" alt="email" src={twitter} />
                       </a>
                   </Col>
+                  <Col md={1}></Col>
       </Row>
       <Row className="space"></Row>
       <hr />
